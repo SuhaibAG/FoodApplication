b1f9cf34dda696e2a50fe904e55e3d59c1ded1b8
Saher <93139459+SaherMarsi@users.noreply.github.com>
2023-11-30 17:42:25 +0300

    Update User.java

--- JunitLab/src/main/java/org/example/User.java
+++ JunitLab/src/main/java/org/example/User.java
@@ -1,81 +1,100 @@
 package org.example;
-import org.junit.*;
 
-import static org.junit.Assert.*;
-
-
-public class tests {
-
-    @Test
-    public void testNutritionalInformationDisplay() {
-        // Assuming you have a class named NutritionalInformationDisplay
-        food plan = new food();
-
-        // Test data
-        String foodItem = "Banana";
-        String expectedNutritionalInfo = "Calories: 105, Carbs: 27g, Protein: 1.3g, Fat: 0.3g, Vitamins: A, B6, C";
-
-        // Set up the application with known data
-        plan.setData(foodItem, expectedNutritionalInfo);
-
-        // Verify the displayed nutritional information matches the expected data
-        assertEquals(expectedNutritionalInfo, plan.getNutritionalInfo());
-    }
-
-    @Test
-    public void testMealPlanGeneration() {
-        //make a list of foods
-        food[] foods = new food[3];
-        foods[0] = new food("Banana", "Calories: 95");
-        foods[1] = new food("Apple", "Calories: 60");
-        foods[2] = new food("Steak", "Calories: 110");
-
-        //check if the foods are registered correctly
-        MealPlan plan = new MealPlan(foods);
-
-        assertEquals(foods, plan.getItems());
-    }
+public class User {
+    private String name;
+    private int age;
+    private String dieteryPreference;
+    private double height;
+    private double weight;
+    private double bmi;
+    private double calories;
 
 
-    @Test
-    public void testBudget(){
-        Budgetting budget = new Budgetting();
-        double amount = 50;
-        budget.accessBudgetPlan();
-        Boolean succesScenario = budget.setBudget(amount);
-
-        //verifies the budget plan
-        assertTrue(succesScenario);
-
+    public User(String name, int age,double height,double weight, String dieteryPreference) {
+        this.name = name;
+        this.age = age;
+        this.dieteryPreference = dieteryPreference;
+        //both the height and weight m and kg.
+        this.height = height;
+        this.weight = weight;
 
     }
 
-    @Test
-    public void testUpdateProfile(){
-        User profile = new User("Rayan", 25, 1.8,78,"Vegan");
-        Account acc = new Account("rayan123", "password123");
 
-        //logging in with username and password
-        acc.login("rayan123", "password123");
-        profile.updatePreferances("Atkins_diet");
-        profile.setAge(27);
-
-        //comment for thing
-        assertEquals("Atkins_diet", profile.getPreferances());
-        assertEquals(27, profile.getAge());
-
-    }
-    @Test
-    public void testCalculator() {
-        User Calculator = new User("Mohammad", 27, 1.75,61,"Default");
-
-        // Test calculateBMI method
-        Calculator.calculateBMI(Calculator.getWeight(),Calculator.getHeight());
-        assertEquals(22.86, Calculator.getBmi(), 0.01); // we used delta for floating-point precision
-
-        // Test calculateCalories method
-        Calculator.calculateCalories(Calculator.getWeight(),Calculator.getHeight(), Calculator.getAge());
-        assertEquals(1768.0, Calculator.getCalories(), 0.01); //same here
+    public double getHeight() {
+        return height;
     }
 
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDieteryPreference() {
+        return dieteryPreference;
+    }
+
+    public void setDieteryPreference(String dieteryPreference) {
+        this.dieteryPreference = dieteryPreference;
+    }
+
+    public void updatePreferances(String atkins_diet) {
+    }
+
+    public String getPreferances() {
+        return getPreferances();
+    }
+
+    // Function to calculate BMI and set the instance variable
+    public void calculateBMI(double weight, double height) {
+        bmi = weight / (height * height);
+        setBmi(bmi);
+    }
+
+    public void calculateCalories(double weight, double height, int age) {
+        // Using Mifflin-St Jeor(....no idea ;) )equation for Basal Metabolic Rate (BMR)
+        double bmr = 10 * weight + 6.25 * height * 100 - 5 * age + 5;
+        // Assuming a sedentary lifestyle, multiplying BMR by 1.2 to get total daily calories
+        calories = bmr * 1.2;
+        setCalories(calories);
+    }
+
+    public double getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(double bmi) {
+        this.bmi = bmi;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
 }
