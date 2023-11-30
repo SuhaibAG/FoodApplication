ab319575804589bfc057bba6476138369f3ceb71
Saher <93139459+SaherMarsi@users.noreply.github.com>
2023-11-30 17:07:59 +0300

    Update User.java

--- JunitLab/src/main/java/org/example/User.java
+++ JunitLab/src/main/java/org/example/User.java
@@ -4,11 +4,65 @@ public class User {
     private String name;
     private int age;
     private String dieteryPreference;
+    private double height;
+    private double weight;
 
 
-    public User(String name, int age, String dieteryPreference) {
+    public User(String name, int age,double height,double weight, String dieteryPreference) {
         this.name = name;
         this.age = age;
         this.dieteryPreference = dieteryPreference;
+        //both the height and weight cm and kg.
+        this.height = height;
+        this.weight = weight;
+
+    }
+
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDieteryPreference() {
+        return dieteryPreference;
+    }
+
+    public void setDieteryPreference(String dieteryPreference) {
+        this.dieteryPreference = dieteryPreference;
+    }
+
+    public void updatePreferances(String atkins_diet) {
+    }
+
+    public String getPreferances() {
+        return getPreferances();
     }
 }
