19fc5d5231bb01345375a9f5fdd690e1f4173743
Saher <93139459+SaherMarsi@users.noreply.github.com>
2023-11-30 17:08:44 +0300

    Create Account.java

---
+++ JunitLab/src/main/java/org/example/Account.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Account {
+    private String username;
+    private String password;
+
+    public Account(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void login(String username, String password) {
+        setUsername(username);
+        setPassword(password);
+    }
+}
