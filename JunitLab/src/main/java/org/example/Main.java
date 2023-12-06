d297ae01a02a3f5588637291f48e0da5ccde3a6c
SuhaibAG <suhaib.ag.49@gmail.com>
2023-12-06 18:40:47 +0300

    Update Main.java

--- JunitLab/src/main/java/org/example/Main.java
+++ JunitLab/src/main/java/org/example/Main.java
@@ -1,9 +1,40 @@
 package org.example;
 import org.junit.Test;
 
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+
 public class Main {
     public static void main(String[] args) {
 
 
     }
+
+
+    public MealPlan GenerateMealPlan() {
+        //make a list of foods
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter how many foods you want to insert in your meal plan");
+        int amount = input.nextInt();
+        food[] foods = new food[amount];
+        for (int i = 0; i < amount; i++) {
+            String name = input.next();
+            String nutrition = input.next();
+            foods[i] = new food(name, nutrition);
+
+        }
+        //check if the foods are registered correctly
+        MealPlan plan = new MealPlan(foods);
+        return plan;
+    }
+
+    public void UpdateProfile(User profile, String diet, int age, int weight){
+
+        profile.updatePreferances(diet);
+        profile.setAge(age);
+        profile.setWeight(weight);
+
+    }
+
 }
